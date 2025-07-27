2010cd0706cb0c7083d91a710e73d9969766a76e
Devanshu3032 <devanshukumar841@gmail.com>
2025-07-27 22:37:05 +0530

    "basic"

---
+++ ContainAndstartWith.java
@@ -0,0 +1,8 @@
+public class ContainAndstartWith {
+    public static void main(String[] args) {
+        String c = "dev" ;
+        System.out.println(c.contains("ev"));
+        System.out.println(c.startsWith("de"));
+    }
+    
+}
