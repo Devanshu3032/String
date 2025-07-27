d8d42fdde00a1e2d2b3b9045479bbc4ec9522135
Devanshu3032 <devanshukumar841@gmail.com>
2025-07-27 23:35:24 +0530

    'Basic'

--- IntToString.java
+++ IntToString.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class IntToString {
     public static void main(String[] args) {
-        
+      String s = "" ;
+      Scanner sc = new Scanner(System.in) ;
+      int a = sc.nextInt() ;
+      s = s+a ;
+      System.out.println(s);
     }
     
 }
