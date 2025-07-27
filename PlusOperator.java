a176574e78eb9ab035e27139b31e831f4677cdfd
Devanshu3032 <devanshukumar841@gmail.com>
2025-07-27 23:24:02 +0530

    "basic"

---
+++ PlusOperator.java
@@ -0,0 +1,10 @@
+public class PlusOperator {
+    public static void main(String[] args) {
+        String s = "devanshu" ;
+        String ss =  "10" ;
+        System.out.println(s + ss);
+        System.out.println(s + 10 + 20);
+        System.out.println(10+20+s);
+    }
+    
+}
