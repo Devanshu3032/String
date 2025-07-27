60f35f68a1b18c38798e9cc44536ca5225b95376
Devanshu3032 <devanshukumar841@gmail.com>
2025-07-27 22:44:03 +0530

    "basic"

---
+++ concatAndtoLowerCase.java
@@ -0,0 +1,14 @@
+public class concatAndtoLowerCase {
+    public static void main(String[] args) {
+        String s = "Devanshu kumar is Goood boy " ;
+        System.out.println(s.toLowerCase());
+        System.out.println(s.toUpperCase());
+
+
+        String ss = "Great boy" ; 
+        System.out.println(s+ss);
+        System.out.println(s.concat(ss));
+    }
+
+    
+}
