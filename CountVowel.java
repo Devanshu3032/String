0d3d0bc07a9ebe056f1a2e1d5a6d61533f2dabea
Devanshu3032 <devanshukumar841@gmail.com>
2025-07-27 22:27:08 +0530

    "Basic"

--- CountVowel.java
+++ CountVowel.java
@@ -1,6 +1,9 @@
+import java.util.*;
 public class CountVowel {
     public static void main(String[] args) {
-        String s = "Devanshu" ;
+        Scanner sc = new Scanner(System.in) ;
+        System.out.println("Enter the word :");
+        String s = sc.nextLine() ;
         String c = "AEIOUaeiou" ;
         int count = 0 ;
         for (int i = 0; i < s.length(); i++) {
---
+++ IndexOfAndCompareTo.java
@@ -0,0 +1,11 @@
+public class IndexOfAndCompareTo {
+    public static void main(String[] args) {
+        String s = "Devansshu Kumar" ;
+        String ss = "Dav" ;
+        System.out.println(s.indexOf('s'));
+        System.out.println(s.lastIndexOf('s'));
+         
+        System.out.println(s.compareTo(ss));
+
+    }
+}
