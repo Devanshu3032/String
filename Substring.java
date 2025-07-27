fd38b07c6f48a7a1726d745181a0f8bab3eeeddb
Devanshu3032 <devanshukumar841@gmail.com>
2025-07-27 22:53:48 +0530

    "basic"

--- Substring.java
+++ Substring.java
@@ -1,5 +1,9 @@
 public class Substring {
     public static void main(String[] args) {
-        
+        String s = "abcde" ;
+        System.out.println(s.substring(1));
+        System.out.println(s.substring(1,4));
+        System.out.println(s.substring(2,2));
+        System.out.println(s.substring(0,5));
     }
 }
