9070684823ce27b76b4da2ec89c9c2dd3e172c80
Devanshu3032 <devanshukumar841@gmail.com>
2025-07-31 23:01:25 +0530

    "had "

---
+++ TotalNumber.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TotalNumber {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in) ;
+        System.out.println("enter The number : ");
+        int n = sc.nextInt();
+        String s = " " + n;
+        System.out.println(s.length());
+        
+    }
+}
