2f609933e1f54131a7efb86e302f58a2ddbc7d6a
Devanshu3032 <devanshukumar841@gmail.com>
2025-08-01 21:25:13 +0530

    "basic "

---
+++ stringUpdation.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class stringUpdation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        System.out.print("Enter a word : ");
+        String d = sc.nextLine() ;
+        String s = "" ;
+         int n = d.length() ;
+         for (int i = 0; i < n; i++) {
+            if(i%2 == 0){
+                s+= e 
+            }
+            
+         }
+        
+    }
+    
+}
