8a74d8baccbc4e0d7b0027ba5cd7d5a08024d689
Devanshu3032 <devanshukumar841@gmail.com>
2025-08-05 20:19:39 +0530

    "basic"

---
+++ stringbuilder.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class stringbuilder {
+public static void main(String[] args) {
+    String s = new String("devanshu");
+    Scanner csc = new Scanner(System.in) ; 
+    StringBuilder x = new StringBuilder("devanshu" ) ;
+    StringBuilder sd = new StringBuilder("daw");
+    
+    StringBuilder sf = new StringBuilder(csc.nextLine()) ;
+    x.setCharAt(1,'a') ;
+    System.out.println(x);
+   }      
+}
